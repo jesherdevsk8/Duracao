5b974083de10b61d46fe09e23b129cbeedc4209b
Jesher Minelli <jesherdevsk8@gmail.com>
2022-03-12 19:33:02 -0300

    nova feature com parametros disponiveis para soma total e uma soma especifica de somente um video

--- duracao.sh
+++ duracao.sh
@@ -14,7 +14,43 @@
 # Exemplo: sudo ln -s $HOME/scripts/duracao.sh /usr/local/bin/duracao
 
 #-------------------VARIÁVEIS
+
+usage="
+  ======= [OPÇÕES] =======
+
+  -h - Help                   |   duracao -h
+  -v - Versão                 |   duracao -v
+  -s - Soma apenas um vídeo   |   duracao -u <Nome_do_Video>
+  -t - Soma Total             |   duracao -t
+"
 total=0
+
+versao="Versão 1.0.1"
+
+#-------------------FUNÇÕES
+
+SOMA_TOTAL(){
+  shopt -s nullglob
+    for video in *.mp4; do
+      duracao=$(ffprobe -i $video -show_entries format=duration -v quiet -of csv="p=0")
+      total=$( echo $total + $duracao | bc )
+    done
+  shopt -u nullglob
+
+  echo "Total de: $( echo "scale=2; $total / 60 / 60" | bc -l ) horas de vídeo"
+}
+
+SOMA_MINUTOS(){
+  echo -e "Vídeos disponíveis\n$(ls *.mp4)\n"
+  read -p "Informe o nome do vídeo MP4: " video #Entrada do usuário
+
+    if [ -z "$video" ];then
+      echo "Não válido, Informe o nome do vídeo" && exit 1
+    else
+      duracao=$(ffprobe -i $video -show_entries format=duration -v quiet -of csv="p=0")
+      echo -e "\nTotal de: $(echo "scale=2; $duracao/60"| bc -l) minutos de vídeo"
+    fi
+}
 
 #-------------------TESTES
 
@@ -22,11 +58,11 @@ total=0
 
 #-------------------EXECUÇÃO
 
-shopt -s nullglob
-  for video in *.mp4; do
-    duracao=$(ffprobe -i $video -show_entries format=duration -v quiet -of csv="p=0")
-    total=$( echo $total + $duracao | bc )
-  done
-shopt -u nullglob
-
-echo "Total de: $( echo "scale=2; $total / 60 / 60" | bc -l ) horas de vídeo"
+  case "$1" in
+    -h) echo "$usage" && exit 0                                   ;;
+    -v) echo "$versao" && exit 0                                  ;;
+    -s) SOMA_MINUTOS                                              ;;
+    -t) SOMA_TOTAL                                                ;;
+     *) echo "Opção inválida, vallie o -h para ajuda." && exit 1  ;;
+  esac
+  shift
